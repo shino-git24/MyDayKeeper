b00ff27010f64537b6403bbf7660f5ed5de4dc1f
shino-git24 <halash0406@gmail.com>
2025-06-08 18:48:35 +0900

    Delete docs/sw.js
    gas上におくことにしたので削除

--- docs/sw.js
+++
@@ -1,8 +0,0 @@
-// Service Workerをインストールするだけの最小限のコード
-self.addEventListener('install', (event) => {
-  // インストール処理
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
-});ｄ
